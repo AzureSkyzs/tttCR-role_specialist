4219ace1d8c07527c9f8544b5b606a329a0ff731
Quinton <redsythc@gmail.com>
2024-04-09 02:41:54 -0500

    Fixed The locate
    The .git was in the wrong locations

---
+++ lua/customroles/specialist.lua
@@ -0,0 +1,33 @@
+local ROLE = {}
+
+ROLE.nameraw = "specialist"
+ROLE.name = "Specialist"
+ROLE.nameplural = ""
+ROLE.nameext = ""
+ROLE.nameshort = ""
+
+ROLE.desc = [[]]
+
+ROLE.team = ROLE_TEAM_
+
+ROLE.shop = nil
+ROLE.loadout = {}
+
+ROLE.startingcredits = nil
+
+ROLE.startinghealth = nil
+ROLE.maxhealth = nil
+
+ROLE.isactive = nil
+ROLE.selectionpredicate = nil
+ROLE.shouldactlikejester = nil
+
+ROLE.translations = {}
+
+ROLE.convars = {}
+
+RegisterRole(ROLE)
+
+if SERVER then
+    AddCSLuaFile()
+end
